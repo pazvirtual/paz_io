df358b6d6f38f4d41179fc3bd477a5bce5e35659
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:44:55 -0500

    Adds file missing from last commit.

---
+++ netpbm.cpp
@@ -0,0 +1,52 @@
+#include "PAZ_IO"
+#include <fstream>
+
+// Need to add support for comments and binary (`P4`) PBMs.
+paz::Image<unsigned int, 1> paz::load_pbm(const std::string& path)
+{
+    std::ifstream in(path);
+    if(!in)
+    {
+        throw std::runtime_error("Failed to open input file \"" + path + "\".");
+    }
+
+    // Check format.
+    char c0, c1;
+    in >> c0;
+    in >> c1;
+    if(c0 != 'P' || c1 != '1')
+    {
+        throw std::runtime_error("This is not a plain PBM file.");
+    }
+
+    // Get dimensions.
+    unsigned int width, height;
+    in >> width;
+    in >> height;
+
+    // Get data.
+    std::vector<unsigned int> pixels(width*height, 0u);
+    char c;
+    unsigned int n = 0;
+    while(in >> c && n < width*height)
+    {
+        if(c == '0')
+        {
+            unsigned int x = n%width;
+            unsigned int y = n/width;
+            pixels[width*(height - 1 - y) + x] = 1u;
+        }
+        if(c == '0' || c == '1')
+        {
+            ++n;
+        }
+    }
+
+    // Check that we got all the pixels.
+    if(n != width*height)
+    {
+        throw std::runtime_error("Number of pixels does not match dimensions.");
+    }
+
+    return paz::Image<unsigned int, 1>(pixels.data(), width, height);
+}
