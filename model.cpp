be40705ef4c3a958c4c2f5d74df00a4a7b350325
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:44:55 -0500

    Changes PAZ archive extension to `*.pazarchive` and adds option for `paz-archive` program to convert each object in each OBJ file into a PAZ model file.

---
+++ model.cpp
@@ -0,0 +1,111 @@
+#include "PAZ_IO"
+#include <sstream>
+
+void paz::parse_model(const Bytes& content, std::vector<float>& positions, std::
+    vector<float>& uvs, std::vector<float>& normals, std::vector<unsigned int>&
+    materials, std::vector<std::string>& materialNames, std::vector<std::
+    string>& materialLibs, std::vector<unsigned int>& indices)
+{
+    std::istringstream iss(content.str());
+
+    positions.clear();
+    uvs.clear();
+    normals.clear();
+    materials.clear();
+    materialNames.clear();
+    materialLibs.clear();
+    indices.clear();
+
+    std::string line;
+
+    std::getline(iss, line);
+    const std::size_t numVerts = std::stoull(line);
+    positions.resize(4*numVerts);
+    uvs.resize(2*numVerts);
+    normals.resize(4*numVerts);
+    materials.resize(numVerts);
+
+    std::getline(iss, line);
+    {
+        std::istringstream iss1(line);
+        while(std::getline(iss1, line, ' '))
+        {
+            materialNames.push_back(line);
+        }
+    }
+
+    std::getline(iss, line);
+    {
+        std::istringstream iss1(line);
+        while(std::getline(iss1, line, ' '))
+        {
+            materialLibs.push_back(line);
+        }
+    }
+
+    for(std::size_t i = 0; i < numVerts; ++i)
+    {
+        std::getline(iss, line);
+        std::istringstream iss1(line);
+        for(int j = 0; j < 3; ++j)
+        {
+            iss1 >> positions[4*i + j];
+        }
+        positions[4*i + 3] = 1.;
+        for(int j = 0; j < 2; ++j)
+        {
+            iss1 >> uvs[2*i + j];
+        }
+        for(int j = 0; j < 3; ++j)
+        {
+            iss1 >> normals[4*i + j];
+        }
+        normals[4*i + 3] = 0.;
+        iss1 >> materials[i];
+    }
+
+    while(std::getline(iss, line))
+    {
+        indices.push_back(std::stoull(line));
+    }
+}
+
+paz::Bytes paz::to_model(std::vector<float>& positions, std::vector<float>& uvs,
+    std::vector<float>& normals, std::vector<unsigned int>& materials, std::
+    vector<std::string>& materialNames, std::vector<std::string>& materialLibs,
+    std::vector<unsigned int>& indices)
+{
+    std::ostringstream oss;
+    oss << materials.size() << '\n';
+    for(std::size_t i = 0; i < materialNames.size(); ++i)
+    {
+        oss << materialNames[i] << (i + 1 < materialNames.size() ? " " : "");
+    }
+    oss << '\n';
+    for(std::size_t i = 0; i < materialLibs.size(); ++i)
+    {
+        oss << materialLibs[i] << (i + 1 < materialLibs.size() ? " " : "");
+    }
+    oss << '\n';
+    for(std::size_t i = 0; i < materials.size(); ++i)
+    {
+        for(int j = 0; j < 3; ++j)
+        {
+            oss << positions[4*i + j] << ' ';
+        }
+        for(int j = 0; j < 2; ++j)
+        {
+            oss << uvs[2*i + j] << ' ';
+        }
+        for(int j = 0; j < 3; ++j)
+        {
+            oss << normals[4*i + j] << ' ';
+        }
+        oss << materials[i] << '\n';
+    }
+    for(auto n : indices)
+    {
+        oss << n << '\n';
+    }
+    return oss.str();
+}
--- util/paz-archive.cpp
+++ util/paz-archive.cpp
@@ -7,16 +7,32 @@
 
 int main(int argc, char** argv)
 {
-    // Figure out if this is a directory and check for valid arguments.
+    // Check for options.
+    const std::string arg1 = argv[1];
+    const bool convert = argc >= 2 && (arg1 == "-c" || arg1 == "--convert");
+
+    // Print help and return if invalid arguments.
+    if(argc < convert + 2)
+    {
+        std::cout << std::endl << "Usage:" << std::endl << "    paz-archive [op"
+            "tions] <files> <output path>" << std::endl << "    paz-archive [op"
+            "tions] <directory> [output path = <directory>.pazarchive]" << std::
+            endl << std::endl << "Options: " << std::endl << "    -c/--convert:"
+            " Convert each OBJ object into a PAZ model" << std::endl << std::
+            endl;
+        return 1;
+    }
+
+    // Figure out if this is a directory.
     std::unordered_set<std::string> files;
     struct stat s;
-    bool isDir = (argc == 2 || argc == 3) && !stat(argv[1], &s) && S_ISDIR(s.
-        st_mode);
+    const bool isDir = (argc == convert + 2 || argc == convert + 3) && !stat(
+        argv[convert + 1], &s) && S_ISDIR(s.st_mode);
     std::string dirPath;
     if(isDir)
     {
         // Extract bare directory path (no trailing slash) for later.
-        dirPath = argv[1];
+        dirPath = argv[convert + 1];
         while(dirPath.back() == '/')
         {
             dirPath.pop_back();
@@ -32,7 +48,7 @@ int main(int argc, char** argv)
 
         DIR* dir;
         struct dirent* ent;
-        if((dir = opendir(argv[1])))
+        if((dir = opendir(argv[convert + 1])))
         {
             while((ent = readdir(dir)))
             {
@@ -46,32 +62,51 @@ int main(int argc, char** argv)
         else
         {
             std::cerr << std::endl << "Error: Unable to open directory \"" <<
-                argv[1] << "\"." << std::endl << std::endl;
+                argv[convert + 1] << "\"." << std::endl << std::endl;
             return 1;
         }
     }
-    else if(argc < 3)
-    {
-        std::cout << std::endl << "Usage:" << std::endl << "    paz-archive [fi"
-            "le 1] ... [file n] [output path]" << std::endl << "    paz-archive"
-            " [directory] (output path - default: [directory].paz)" << std::endl
-            << std::endl;
-        return 1;
-    }
     else
     {
-        files = std::unordered_set<std::string>(argv + 1, argv + argc - 1);
+        files = std::unordered_set<std::string>(argv + convert + 1, argv + argc
+            - 1);
     }
 
     // Compress input files.
     paz::Archive archive;
     for(const auto& n : files)
     {
-        archive.add(n, paz::read_bytes(isDir ? dirPath + "/" + n : n));
+        auto contents = paz::read_bytes(isDir ? dirPath + "/" + n : n);
+        if(convert && n.size() > 4 && n.substr(n.size() - 4) == ".obj")
+        {
+            std::vector<std::string> names;
+            std::vector<std::vector<float>> positions;
+            std::vector<std::vector<float>> uvs;
+            std::vector<std::vector<float>> normals;
+            std::vector<std::vector<unsigned int>> materials;
+            std::vector<std::string> materialNames;
+            std::vector<std::string> materialLibs;
+            std::vector<std::vector<unsigned int>> indices;
+            paz::parse_obj(contents, names, positions, uvs, normals, materials,
+                materialNames, materialLibs, indices);
+            for(std::size_t i = 0; i < names.size(); ++i)
+            {
+                const std::string path = n.substr(0, n.size() - 4) + (names.
+                    size() > 1 ? "_" + names[i] : "") + ".pazmodel";
+                std::cout << "Adding \"" << path << "\"" << std::endl;
+                archive.add(path, paz::to_model(positions[i], uvs[i], normals[
+                    i], materials[i], materialNames, materialLibs, indices[i]));
+            }
+        }
+        else
+        {
+            std::cout << "Adding \"" << n << "\"" << std::endl;
+            archive.add(n, contents);
+        }
     }
 
     // Create archive.
-    const std::string outPath = (isDir && argc < 3) ? dirPath + ".paz" : argv[
-        argc - 1];
+    const std::string outPath = (isDir && argc < convert + 3) ? dirPath +
+        ".pazarchive" : argv[argc - 1];
     archive.write(outPath);
 }
