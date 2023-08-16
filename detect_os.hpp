761308eb429f060d439cf9c9ef2637112217fb66
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:44:55 -0500

    Minor update (fixes a warning).

--- detect_os.hpp
+++ detect_os.hpp
@@ -2,7 +2,7 @@
 #define PAZ_IO_DETECT_OS_HPP
 
 #if defined(__APPLE__) && defined(__MACH__)
-#if TARGET_OS_IPHONE == 0
+#if !defined(TARGET_OS_IPHONE) || !TARGET_OS_IPHONE
 #define PAZ_MACOS
 #else
 static_assert(false, "Failed to detect an acceptable OS (iOS is not supported)."
