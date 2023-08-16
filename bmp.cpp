6c8d89acfc7d26bcc6979c484c0369343d2f7414
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:44:55 -0500

    Adds BMP code improvements.

--- bmp.cpp
+++ bmp.cpp
@@ -6,7 +6,7 @@
 paz::Image<std::uint8_t, 4> paz::parse_bmp(const Bytes& content)
 {
     std::string fmt;
-    if(content.size() >= 15)
+    if(content.size() > 1)
     {
         fmt = std::string(content.begin(), content.begin() + 2);
     }
@@ -31,8 +31,8 @@ paz::Image<std::uint8_t, 4> paz::parse_bmp(const Bytes& content)
     }
     if(headerSize != 40)
     {
-        throw std::runtime_error("Unsupported DIB header format (must be BITMAP"
-            "INFOHEADER).");
+        throw std::runtime_error("Unsupported DIB header format " + std::
+            to_string(headerSize) + " (must be 40/BITMAPINFOHEADER).");
     }
     long width = 0;
     for(int i = 0; i < 4; ++i)
@@ -79,41 +79,44 @@ paz::Bytes paz::to_bmp(const Image<std::uint8_t, 4>& img)
         static_cast<unsigned long>(img.width()), static_cast<unsigned long>(img.
         height()), 0, 0, paddedSize, 0, 0, 0, 0};
 
-    Bytes res("BM");
+    Bytes res(54 + paddedSize);
+    res[0] = 'B';
+    res[1] = 'M';
 
     for(int i = 0; i < 6; ++i)
     {
-        res.push_back(headers[i]&0x000000ffu);
-        res.push_back((headers[i]&0x0000ff00u) >> 8);
-        res.push_back((headers[i]&0x00ff0000u) >> 16);
-        res.push_back((headers[i]&0xff000000u) >> 24);
+        res[2 + 4*i + 0] = headers[i]&0x000000ffu;
+        res[2 + 4*i + 1] = (headers[i]&0x0000ff00u) >> 8;
+        res[2 + 4*i + 2] = (headers[i]&0x00ff0000u) >> 16;
+        res[2 + 4*i + 3] = (headers[i]&0xff000000u) >> 24;
     }
 
-    res.push_back(1);
-    res.push_back(0);
-    res.push_back(24);
-    res.push_back(0);
+    res[26] = 1;
+    res[27] = 0;
+    res[28] = 24;
+    res[29] = 0;
 
     for(int i = 7; i < 13; ++i)
     {
-        res.push_back(headers[i]&0x000000ffu);
-        res.push_back((headers[i]&0x0000ff00u) >> 8);
-        res.push_back((headers[i]&0x00ff0000u) >> 16);
-        res.push_back((headers[i]&0xff000000u) >> 24);
+        res[2 + 4*i + 0] = headers[i]&0x000000ffu;
+        res[2 + 4*i + 1] = (headers[i]&0x0000ff00u) >> 8;
+        res[2 + 4*i + 2] = (headers[i]&0x00ff0000u) >> 16;
+        res[2 + 4*i + 3] = (headers[i]&0xff000000u) >> 24;
     }
 
     for(int y = 0; y < img.height(); ++y)
     {
         for(int x = 0; x < img.width(); ++x)
         {
-            for(int i = 2; i >= 0; --i)
+            for(int i = 0; i < 3; ++i)
             {
-                res.push_back(img[4*(y*img.width() + x) + i]);
+                res[54 + (3*img.width() + extraBytes)*y + 3*x + i] = img[4*(y*
+                    img.width() + x) + 2 - i];
             }
         }
         for(unsigned int i = 0; i < extraBytes; ++i)
         {
-            res.push_back(0);
+            res[54 + (3*img.width() + extraBytes)*y + 3*img.width() + i] = 0;
         }
     }
 
